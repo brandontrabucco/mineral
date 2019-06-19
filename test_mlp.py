4beef70a01c19a3ea57f6f8590e29d6a20e934e3
Brandon <brandon@btrabucco.com>
2019-06-18 20:44:25 -0700

    updated tests

--- test_mlp.py
+++ test_mlp.py
@@ -2,18 +2,23 @@
 
 
 import tensorflow as tf
-from jetpack.networks.dense_mlp import DensePolicy, DenseQF
+from jetpack.networks.dense_policy import DensePolicy
+from jetpack.networks.dense_qf import DenseQF
+from jetpack.networks.dense_vf import DenseVF
 
 
 if __name__ == "__main__":
-
 
     policy = DensePolicy(
         [6, 6, 1]
     )
 
     qf = DenseQF(
-        [6, 6]
+        [6, 6, 1]
+    )
+
+    vf = DenseVF(
+        [6, 6, 1]
     )
 
     x = tf.random.normal([32, 6])
@@ -34,3 +39,11 @@ if __name__ == "__main__":
             qf.minimize(qf_loss, tape)
 
         print("qf_loss", qf_loss)
+
+    for i in range(100):
+
+        with tf.GradientTape() as tape:
+            vf_loss = tf.reduce_mean(tf.losses.mean_squared_error(y, vf(x)))
+            qf.minimize(vf_loss, tape)
+
+        print("vf_loss", vf_loss)
