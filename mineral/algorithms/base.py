f74aef3bae6d143614e098cf638772501e4564f9
Brandon <brandon@btrabucco.com>
2019-08-12 22:50:12 -0700

    added update after flag

--- mineral/algorithms/base.py
+++ mineral/algorithms/base.py
@@ -9,10 +9,12 @@ class Base(ABC):
     def __init__(
         self,
         update_every=1,
+        update_after=1,
         selector=None,
         monitor=None,
     ):
         self.update_every = update_every
+        self.update_after = update_after
         self.selector = (lambda x: x) if selector is None else selector
         self.monitor = monitor
         self.iteration = 0
@@ -36,7 +38,8 @@ class Base(ABC):
         terminals
     ):
         self.iteration += 1
-        if self.iteration - self.last_update_iteration >= self.update_every:
+        if (self.iteration >= self.update_after) and (
+                self.iteration - self.last_update_iteration >= self.update_every):
             self.last_update_iteration = self.iteration
             self.update_algorithm(
                 self.selector(observations),
