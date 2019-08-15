e22ca5579a37e934a72f2549e225620f0e23e174
Brandon Trabucco <brandon@btrabucco.com>
2019-08-15 15:23:11 -0700

    added hindsight relabeling

---
+++ mineral/buffers/relabelers/hindsight_relabeler.py
@@ -0,0 +1,64 @@
+"""Author: Brandon Trabucco, Copyright 2019"""
+
+
+import tensorflow as tf
+from mineral.buffers.relabelers.relabeler import Relabeler
+
+
+def default_goal_assigner(x, y):
+    y["goal"] = x
+
+
+class HindsightRelabeler(Relabeler):
+    
+    def __init__(
+        self,
+        buffer,
+        time_skip=1,
+        relabel_probability=1.0,
+        observation_selector=(lambda x: x["proprio_observation"]),
+        goal_selector=(lambda x: x["goal"]),
+        goal_assigner=default_goal_assigner
+    ):
+        Relabeler.__init__(self, buffer)
+        self.time_skip = time_skip
+        self.relabel_probability = relabel_probability
+        self.observation_selector = observation_selector
+        self.goal_selector = goal_selector
+        self.goal_assigner = goal_assigner
+
+    def relabel(
+        self,
+        observations,
+        actions,
+        rewards,
+        terminals
+    ):
+        selected_observations = self.observation_selector(
+            observations)
+        indices = (tf.tile(
+            tf.expand_dims(tf.range(tf.shape(selected_observations)[1]), 0),
+            [tf.shape(selected_observations)[0], 1])
+                // self.time_skip) * self.time_skip
+        achieved_goals = tf.gather(
+            selected_observations,
+            indices,
+            batch_dims=2)
+        original_goals = self.goal_selector(observations)
+        relabel_condition = tf.broadcast_to(
+                self.relabel_probability > tf.random.uniform(
+                    tf.shape(selected_observations)[:2],
+                    maxval=1.0,
+                    dtype=tf.float32),
+                tf.shape(achieved_goals))
+        relabeled_goals = tf.where(
+            relabel_condition,
+            achieved_goals,
+            original_goals)
+        relabeled_observations = self.goal_assigner(
+            relabeled_goals, selected_observations)
+        return (
+            relabeled_observations,
+            actions,
+            rewards,
+            terminals)
